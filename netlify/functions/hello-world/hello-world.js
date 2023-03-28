8b5a6383b3f33daa7ff6c78d9f39af3da71e4e56
Kit <liwkcuhk+gitlab@gmail.com>
2023-03-28 21:15:41 +0800

    helloWorld

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,17 +1,35 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+const chromium = require('chrome-aws-lambda');
 
-module.exports = { handler }
+exports.handler = async (event, context) => {
+
+    const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+
+    if (!pageToScreenshot) return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Page URL not defined' })
+    }
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+    });
+    
+    const page = await browser.newPage();
+
+    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+    const screenshot = await page.screenshot({ encoding: 'binary' });
+
+    await browser.close();
+  
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ 
+            message: `Complete screenshot of ${pageToScreenshot}`, 
+            buffer: screenshot 
+        })
+    }
+
+}
---
+++ up.bat
@@ -0,0 +1,3 @@
+git add *
+git commit -m "helloWorld"
+git push  https://github.com/liwkss/angular-quickstart
