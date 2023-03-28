49bde438dad0f2cdce19a3efb1c32dd2b0820189
Kit <liwkcuhk+gitlab@gmail.com>
2023-03-28 21:44:01 +0800

    helloWorld

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,13 +1,9 @@
 const chromium = require('chrome-aws-lambda');
 
 exports.handler = async (event, context) => {
+    var ret="";
 
-    const pageToScreenshot = JSON.parse(event.body).pageToScreenshot;
 
-    if (!pageToScreenshot) return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'Page URL not defined' })
-    }
 
     const browser = await chromium.puppeteer.launch({
         args: chromium.args,
@@ -16,20 +12,21 @@ exports.handler = async (event, context) => {
         headless: chromium.headless,
     });
     
-    const page = await browser.newPage();
+	const page = await browser.newPage()
 
-    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+    await page.goto('https://www.hkab.org.hk/hibor/listRates.do?lang=en&Submit=Detail')
 
-    const screenshot = await page.screenshot({ encoding: 'binary' });
+	const text = await page.evaluate(() => Array.from(document.querySelectorAll("body > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr > td > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(3) > td:nth-child(1) > table > tbody > tr > td > table > tbody > tr:nth-child(n+4):nth-last-child(n+3) > td"), element => element.textContent));
+    for(var i=0;i<text.length/2;i++){
+		//console.log(text[2*i]+"\t"+text[2*i+1])
+		ret = ret+ text[2*i+1]+",";
+	}
 
     await browser.close();
-  
+	
     return {
         statusCode: 200,
-        body: JSON.stringify({ 
-            message: `Complete screenshot of ${pageToScreenshot}`, 
-            buffer: screenshot 
-        })
+        body: ret,
     }
 
 }
